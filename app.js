1ec7e83da18223510c3e1d33fa25b0743f9085c4
HenrikH <henrik.hellem@live.com>
2023-02-13 14:22:54 +0100

    fix path typo

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.locals.appTitle = "Created with IronLauncher";
 const indexRoutes = require("./routes/index.routes");
 app.use("/", indexRoutes);
 
-const authRoutes = require(".routes/auth.routes")
+const authRoutes = require("./routes/auth.routes")
 app.use("/", authRoutes)
 
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
