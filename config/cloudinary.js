3a9530c830c0861e093eb4276aee3af1f1d78312
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-13 17:46:10 +0100

    Add ability to add images (to cloudinary)

---
+++ config/cloudinary.js
@@ -0,0 +1,24 @@
+const cloudinary = require("cloudinary").v2;
+const { CloudinaryStorage } = require("multer-storage-cloudinary");
+const multer = require("multer");
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.API_KEY,
+  api_secret: process.env.API_SECRET,
+});
+
+const storage = new CloudinaryStorage({
+  cloudinary: cloudinary,
+  params: {
+    folder: "movie-uploads",
+    allowed_formats: "jpg, png",
+  },
+});
+
+const uploader = multer({ storage });
+
+module.exports = {
+  uploader,
+  cloudinary,
+};
