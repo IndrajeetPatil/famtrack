327d676a63a3bf50a8318ed2d076698473c752f0
HenrikH <henrik.hellem@live.com>
2023-02-14 23:00:07 +0100

    Add basic routing for member/create

--- routes/family-member.js
+++ routes/family-member.js
@@ -1,6 +1,60 @@
 const express = require("express");
 const router = express.Router();
+const { uploader, cloudinary } = require("../config/cloudinary");
 
-router.get("/family/member/create", (req,res,next) => res.render("member/create"))
+const FamilyMember = require("../models/FamilyMember")
+
+router.get("/family/member/create", (req, res, next) =>
+  res.render("member/create")
+);
+
+router.post(
+  "/family/member/create",
+  uploader.single("image"),
+  (req, res, next) => {
+    console.log(req.file);
+
+    const {
+      firstName,
+      lastName,
+      sex,
+      dateOfBirth,
+      placeOfBirth,
+      dateOfDeath,
+      placeOfDeath,
+      parent,
+      sibling,
+      child,
+    } = req.body;
+
+    const imgName = req.file.originalname;
+    const imgPath = req.file.path;
+    const publicId = req.file.filename;
+
+    FamilyMember.create({       
+        firstname,
+        lastName,
+        sex,
+        dateOfBirth,
+        placeOfBirth,
+        dateOfDeath,
+        placeOfDeath,
+        relationship,
+        parent,
+        sibling,
+        child, 
+        imgName, 
+        imgPath, 
+        publicId })
+      .then((familyMember) => {
+        console.log(familyMember);
+        // TODO: Fix redirect here
+        res.redirect("/family/member/id");
+      })
+      .catch((err) => {
+        next(err);
+      });
+  }
+);
 
 module.exports = router;
