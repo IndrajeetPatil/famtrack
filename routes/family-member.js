481d526d66318b82d65ff3d4e8b915e4e6386fe8
HenrikH <henrik.hellem@live.com>
2023-02-14 22:31:07 +0100

    Bootstrap create.hbs

--- routes/family-member.js
+++ routes/family-member.js
@@ -1,4 +1,6 @@
 const express = require("express");
 const router = express.Router();
 
+router.get("/family/member/create", (req,res,next) => res.render("member/create"))
+
 module.exports = router;
