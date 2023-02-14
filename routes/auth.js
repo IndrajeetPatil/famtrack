a28639246da52378edfa63d9502cd61933a1ba58
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-14 17:05:29 +0100

    Error message cleanup (#29)
    * create a new object for errors
    
    * inline returns
    
    * provide feedback also on login page
    
    * make sure users accepts terms and conds
    
    * inline
    
    * reformat
    
    * undo removal of mongoose validation error

--- routes/auth.js
+++ routes/auth.js
@@ -3,6 +3,8 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const duplicateKeyErrorCode = 11000;
 
+const { errors, signalBadInput } = require("../utils/errors");
+
 // ℹ️ Handles password encryption
 const bcrypt = require("bcrypt");
 const saltRounds = 10;
@@ -22,37 +24,27 @@ router.post("/auth/signup", isLoggedOut, (req, res) => {
   const { username, email, password } = req.body;
 
   // Check that username, email, and password are provided
+  // Although these fields are required in the HTML form, we still need to check here
+  // in case the user manipulates the HTML and removes the required attribute
   if (username === "" || email === "" || password === "") {
-    return res.status(400).render("index", {
-      errorMessage: "All fields are mandatory. Please provide your username, email, and password.",
-    });
+    return signalBadInput(res, "index", errors.mandatorySignupFieldsMissing);
   }
+
+  // Validate user has agreed to terms and conditions
+  if (req.body.terms !== "on") return signalBadInput(res, "index", errors.termsNotAccepted);
 
   // Validate uniqueness of username and email
   User.findOne({ username }).then((user) => {
-    if (user) {
-      return res.status(400).render("index", {
-        errorMessage: "Username already exists. Please provide a different username.",
-      });
-    }
+    if (user) return signalBadInput(res, "index", errors.duplicateUsername);
   });
 
   User.findOne({ email }).then((user) => {
-    if (user) {
-      return res.status(400).render("index", {
-        errorMessage: "Email already exists. Please provide a different email.",
-      });
-    }
+    if (user) return signalBadInput(res, "index", errors.duplicateEmail);
   });
 
   // This regular expression checks password for special characters and minimum length
   const regex = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
-  if (!regex.test(password)) {
-    return res.status(400).render("index", {
-      errorMessage:
-        "Password needs to have at least 6 characters and one number, one lowercase and one uppercase letter.",
-    });
-  }
+  if (!regex.test(password)) return signalBadInput(res, "index", errors.weakPassword);
 
   // Create a new user in the database
   bcrypt
@@ -67,10 +59,6 @@ router.post("/auth/signup", isLoggedOut, (req, res) => {
     .catch((error) => {
       if (error instanceof mongoose.Error.ValidationError) {
         res.status(500).render("index", { errorMessage: error.message });
-      } else if (error.code === duplicateKeyErrorCode) {
-        res.status(500).render("index", {
-          errorMessage: "Username and email need to be unique. Provide a valid username or email.",
-        });
       } else {
         next(error);
       }
@@ -86,40 +74,22 @@ router.post("/auth/login", isLoggedOut, (req, res, next) => {
 
   // Check that username and password are provided
   if (username === "" || password === "") {
-    return res.status(400).render("auth/login", {
-      errorMessage: "All fields are mandatory. Please provide username and password.",
-    });
-  }
-
-  // Here we use the same logic as above
-  // - either length based parameters or we check the strength of a password
-  if (password.length < 6) {
-    return res.status(400).render("auth/login", {
-      errorMessage: "Your password needs to be at least 6 characters long.",
-    });
+    return signalBadInput(res, "auth/login", errors.mandatoryLoginFieldsMissing);
   }
 
   // Check if the user exists in the database using email
   User.findOne({ username })
     .then((user) => {
-      // If the user isn't found, send an error message that user provided wrong credentials
-      // TODO: Should we provide a way for users to reset their password? (very low priority)
-      if (!user) {
-        return res.status(400).render("auth/login", { errorMessage: "Wrong credentials." });
-      }
+      if (!user) return signalBadInput(res, "auth/login", errors.userNotFound);
 
-      // If user is found based on the username, check if the in putted password matches the one saved in the database
+      // If user is found, check if the entered password matches the database one
+      // If it does, add the user object (minus password) to the session object
       bcrypt
         .compare(password, user.password)
         .then((isSamePassword) => {
-          if (!isSamePassword) {
-            return res.status(400).render("auth/login", { errorMessage: "Wrong credentials." });
-          }
-
-          // Add the user object (minus password) to the session object
+          if (!isSamePassword) return signalBadInput(res, "auth/login", errors.wrongPassword);
           req.session.currentUser = user.toObject();
           delete req.session.currentUser.password;
-
           user.family ? res.redirect(`/family/${user.family._id}`) : res.redirect("/start");
         })
         .catch((err) => next(err));
@@ -130,9 +100,7 @@ router.post("/auth/login", isLoggedOut, (req, res, next) => {
 // GET /auth/logout
 router.get("/logout", isLoggedIn, (req, res) => {
   req.session.destroy((err) => {
-    if (err) {
-      return res.status(500).render("auth/logout", { errorMessage: err.message });
-    }
+    if (err) return res.status(500).render("auth/logout", { errorMessage: err.message });
     res.redirect("/");
   });
 });
