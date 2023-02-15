ecd300346d46dba9c188c9922f4bc2063b9c7159
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-15 09:48:14 +0100

    edit and delete routes

--- routes/member.js
+++ routes/member.js
@@ -10,13 +10,41 @@ const User = require("../models/User");
 const isLoggedOut = require("../middleware/isLoggedOut");
 const isLoggedIn = require("../middleware/isLoggedIn");
 
-/* GET home page */
-router.get("/family/member/", isLoggedOut, (req, res, next) => res.render("index"));
+router.get("/family/member/", isLoggedOut, (req, res) => res.render("./member/details"));
 
 router.get("/family/member/:id", isLoggedIn, (req, res, next) => {
   User.findById(req.params.id)
-    .populate("family")
+    //.populate("family")
     .then((user) => res.render("member/details", { user }))
+    .catch((err) => next(err));
+});
+
+router.get("/family/member/:id/edit", isLoggedIn, (req, res, next) => {
+  User.findById(req.params.id)
+    //.populate("family")
+    .then((user) => res.render("member/edit", { user }))
+    .catch((err) => next(err));
+});
+
+router.post("/family/member/:id/edit", uploader.single("family-member-photo"), (req, res, next) => {
+  const imgName = req.file.originalname;
+  const imgPath = req.file.path;
+  const publicId = req.file.filename;
+
+  User.findByIdAndUpdate(req.params.id, { ...req.body, imgName, imgPath, publicId })
+    .then((user) => res.redirect(`/family/member/${req.params.id}`))
+    .catch((err) => next(err));
+});
+
+router.get("/family/member/:id/delete", isLoggedIn, (req, res, next) => {
+  User.findByIdAndDelete(req.params.id)
+    .then((user) => res.redirect("/overview"))
+    .catch((err) => next(err));
+});
+
+router.post("/family/member/:id/delete", (req, res, next) => {
+  User.findByIdAndDelete(req.params.id)
+    .then((user) => res.redirect("/overview"))
     .catch((err) => next(err));
 });
 
