65df6dc27f6b3c1b5013cea1e42b094fb5de7d20
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-15 13:51:40 +0100

    run formatter; follow kebab case convention

--- routes/family-member.js
+++ routes/family-member.js
@@ -6,40 +6,29 @@ const axios = require("axios");
 
 const FamilyMember = require("../models/FamilyMember");
 
-router.get("/family/member/create", (req, res, next) =>
-  res.render("member/create")
-);
+router.get("/family/member/create", (req, res) => res.render("member/create"));
 
-router.post(
-  "/family/member/create",
-  uploader.single("memberImg"),
-  async (req, res, next) => {
-    try {
-      let { imgName = "", imgPath = "", publicId = "" } = {};
+router.post("/family/member/create", uploader.single("member-image"), async (req, res, next) => {
+  try {
+    let { imgName = "", imgPath = "", publicId = "" } = {};
 
-      if (!req.file) {
-        const response = await axios.get(
-          `https://ui-avatars.com/api/?background=random&name=${req.body.firstName}+${req.body.lastName}&format=svg`
-        );
-        imgPath = response.config.url;
-      } else {
-        imgName = req.file.originalname;
-        imgPath = req.file.path;
-        publicId = req.file.filename;
-      }
-
-      const familyMember = await FamilyMember.create({
-        ...req.body,
-        imgName,
-        imgPath,
-        publicId,
-      });
-
-      res.redirect(`/family/member/${familyMember._id}`);
-    } catch (err) {
-      next(err);
+    if (!req.file) {
+      const response = await axios.get(
+        `https://ui-avatars.com/api/?background=random&name=${req.body.firstName}+${req.body.lastName}&format=svg`
+      );
+      imgPath = response.config.url;
+    } else {
+      imgName = req.file.originalname;
+      imgPath = req.file.path;
+      publicId = req.file.filename;
     }
+
+    const familyMember = await FamilyMember.create({ ...req.body, imgName, imgPath, publicId });
+
+    res.redirect(`/family/member/${familyMember._id}`);
+  } catch (err) {
+    next(err);
   }
-);
+});
 
 module.exports = router;
