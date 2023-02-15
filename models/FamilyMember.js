9529525fe09e9dd37ab0c5823029b2f05409da7e
Henrik Hellem <57460240+TheHellem@users.noreply.github.com>
2023-02-15 13:56:26 +0100

    Family member create (#31)
    * Basic html view for family/member/create
    
    * Change index back to index
    
    * Add some bootstrap
    
    * Bootstrap create.hbs
    
    * Add basic routing for member/create
    
    * Remove undesired object field
    
    * Add image for member either upload or UI avatar
    
    * Refactor and improve readability using the spread operator
    
    * Fix bug and update redirect
    
    * Add family member without family and relations
    
    * Remove console.log
    
    * run formatter; follow kebab case convention
    
    * Update cloudinary.js
    
    * formatting hbs
    
    ---------
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>

--- models/FamilyMember.js
+++ models/FamilyMember.js
@@ -12,7 +12,7 @@ const familyMemberSchema = new mongoose.Schema({
   sex: {
     type: String,
     required: true,
-    enum: ["Male", "Female", "Other"],
+    enum: ["Male", "Female", "Non-binary"],
   },
   dateOfBirth: {
     type: Date,
@@ -20,6 +20,10 @@ const familyMemberSchema = new mongoose.Schema({
   },
   placeOfBirth: {
     type: String,
+    required: false,
+  },
+  dateOfDeath: {
+    type: Date,
     required: false,
   },
   placeOfDeath: {
@@ -37,7 +41,7 @@ const familyMemberSchema = new mongoose.Schema({
   ],
   relationship: {
     type: String,
-    required: true,
+    required: false,
     enum: ["Parent", "Sibling", "Child"],
   },
   parent: {
--- routes/family-member.js
+++ routes/family-member.js
@@ -1,4 +1,34 @@
 const express = require("express");
 const router = express.Router();
+const { uploader, cloudinary } = require("../config/cloudinary");
+const isLoggedOut = require("../middleware/isLoggedOut");
+const axios = require("axios");
+
+const FamilyMember = require("../models/FamilyMember");
+
+router.get("/family/member/create", (req, res) => res.render("member/create"));
+
+router.post("/family/member/create", uploader.single("memberImage"), async (req, res, next) => {
+  try {
+    let { imgName = "", imgPath = "", publicId = "" } = {};
+
+    if (!req.file) {
+      const response = await axios.get(
+        `https://ui-avatars.com/api/?background=random&name=${req.body.firstName}+${req.body.lastName}&format=svg`
+      );
+      imgPath = response.config.url;
+    } else {
+      imgName = req.file.originalname;
+      imgPath = req.file.path;
+      publicId = req.file.filename;
+    }
+
+    const familyMember = await FamilyMember.create({ ...req.body, imgName, imgPath, publicId });
+
+    res.redirect(`/family/member/${familyMember._id}`);
+  } catch (err) {
+    next(err);
+  }
+});
 
 module.exports = router;
