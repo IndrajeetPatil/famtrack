88ba683d82043c40fc8831889ba218367209386e
HenrikH <henrik.hellem@live.com>
2023-02-15 13:28:31 +0100

    Add family member without family and relations

--- routes/family-member.js
+++ routes/family-member.js
@@ -2,20 +2,25 @@ const express = require("express");
 const router = express.Router();
 const { uploader, cloudinary } = require("../config/cloudinary");
 const isLoggedOut = require("../middleware/isLoggedOut");
-const axios = require("axios")
+const axios = require("axios");
 
-const FamilyMember = require("../models/FamilyMember")
+const FamilyMember = require("../models/FamilyMember");
 
 router.get("/family/member/create", (req, res, next) =>
   res.render("member/create")
 );
 
-router.post("/family/member/create", uploader.single("memberImg"), async (req, res, next) => {
+router.post(
+  "/family/member/create",
+  uploader.single("memberImg"),
+  async (req, res, next) => {
     try {
-        let { imgName = "", imgPath = "", publicId = "" } = {};
-      
+      let { imgName = "", imgPath = "", publicId = "" } = {};
+
       if (!req.file) {
-        const response = await axios.get(`https://ui-avatars.com/api/?background=random&name=${req.body.firstName}+${req.body.lastName}&format=svg`);
+        const response = await axios.get(
+          `https://ui-avatars.com/api/?background=random&name=${req.body.firstName}+${req.body.lastName}&format=svg`
+        );
         imgPath = response.config.url;
       } else {
         imgName = req.file.originalname;
@@ -28,14 +33,14 @@ router.post("/family/member/create", uploader.single("memberImg"), async (req, r
         imgName,
         imgPath,
         publicId,
-      })
-  
+      });
+
       res.redirect(`/family/member/${familyMember._id}`);
     } catch (err) {
       console.log(err);
       next(err);
     }
-  });
-  
+  }
+);
 
 module.exports = router;
