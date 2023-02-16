2d78500df11ffa993103baec12c8882c2b407421
binghatch <aron.seiffert@gmail.com>
2023-02-16 20:37:44 +0100

    feat: add mapbox map

--- public/js/script.js
+++ public/js/script.js
@@ -2,3 +2,11 @@
 // document.addEventListener("DOMContentLoaded", () => {
 //   console.log("famtrack JS imported successfully!");
 // });
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmluZ2hhdGNoIiwiYSI6ImNsZTdiYzQzaDAzeWMzc3F4b3piMWEzZHgifQ.k4mj9dujH6ulPhWNTCyuYA';
+const map = new mapboxgl.Map({
+    container: 'map', // container ID
+    style: 'mapbox://styles/mapbox/streets-v12', // style URL
+    center: [-74.5, 40], // starting position [lng, lat]
+    zoom: 9 // starting zoom
+});
