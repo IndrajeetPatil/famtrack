f1e4391fc2e9320cfd214c68c45bb977e36791a8
binghatch <aron.seiffert@gmail.com>
2023-02-16 10:33:46 +0100

    Adds family members to family details view (#52)
    * style: change card style to border-less and add shadow
    
    * feat: add family members to family details, smaller design adjustments, add utils/calculations for calculation functions
    
    * Update routes/family.js
    
    Add parentheses around arguments of arrow functions.
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>
    
    * Update routes/family.js
    
    Add parentheses around arguments of arrow functions.
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>
    
    * Update utils/calculations.js
    
    create millisecondToYearConversion variable
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>
    
    * Update family.js
    
    Add `.catch()` to router.get
    
    ---------
    
    Co-authored-by: Indrajeet Patil <patilindrajeet.science@gmail.com>

--- routes/family.js
+++ routes/family.js
@@ -8,7 +8,10 @@ const User = require("../models/User");
 const FamilyMember = require("../models/FamilyMember");
 const LifeEvent = require("../models/LifeEvent");
 
+const { calculateAgeFromBirthdate, calculateEarliestBirthyear } = require('../utils/calculations');
+
 const { uploader, cloudinary } = require("../config/cloudinary");
+
 
 router.get("/family/create", isLoggedIn, (req, res, next) => {
   res.render("family/create");
@@ -34,5 +37,27 @@ router.post("/family/create", isLoggedIn, (req, res, next) => {
     })
     .catch((err) => next(err));
 });
+
+router.get("/family/:familyId", isLoggedIn, (req, res, next) => {
+  const familyId = req.params.familyId;
+
+  Family.findById(familyId)
+    .populate("familyMembers")
+    .then((family) => {
+      const numberOfMembers = family.familyMembers.length;
+
+      // Calculates age and adds it to the member object as "age" property
+      family.familyMembers.forEach((member) => {
+        const age = calculateAgeFromBirthdate(member.dateOfBirth);
+        member.age = age;
+      })
+
+      // Get earliest birth year
+      const earliestBirthyear = calculateEarliestBirthyear(family.familyMembers);
+
+      return res.render("family/details", { members: family.familyMembers, numberOfMembers, earliestBirthyear })
+    })
+    .catch((err) => next(err));
+})
 
 module.exports = router;
---
+++ utils/calculations.js
@@ -0,0 +1,19 @@
+const calculateAgeFromBirthdate = (birthdate) => {
+  const today = new Date();
+  const millisecondToYearConversion = 1000 / 60 / 60 / 24 / 365;
+  let age = Math.floor((today - new Date(birthdate)) / millisecondToYearConversion);
+  return age;
+}
+
+const calculateEarliestBirthyear = (familyMembers) => {
+  const membersSortedByBirthyear = familyMembers.sort((next, current) => {
+    return new Date(current.dateOfBirth).getUTCFullYear() - new Date(next.dateOfBirth).getUTCFullYear();
+  });
+  return new Date(membersSortedByBirthyear[0].dateOfBirth).getUTCFullYear();
+}
+
+
+module.exports = {
+  calculateAgeFromBirthdate,
+  calculateEarliestBirthyear
+}
