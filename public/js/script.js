0295933137f49d36fddda35ad9a9b90343286954
Indrajeet Patil <patilindrajeet.science@gmail.com>
2023-02-17 11:04:31 +0100

    correct order

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,7 @@
-mapboxgl.accessToken = 'pk.eyJ1IjoiYmluZ2hhdGNoIiwiYSI6ImNsZTdiYzQzaDAzeWMzc3F4b3piMWEzZHgifQ.k4mj9dujH6ulPhWNTCyuYA';
+mapboxgl.accessToken = "pk.eyJ1IjoiYmluZ2hhdGNoIiwiYSI6ImNsZTdiYzQzaDAzeWMzc3F4b3piMWEzZHgifQ.k4mj9dujH6ulPhWNTCyuYA";
 const map = new mapboxgl.Map({
-    container: 'map', // container ID
-    style: 'mapbox://styles/mapbox/streets-v12', // style URL
-    center: [-74.5, 40], // starting position [lng, lat]
-    zoom: 9 // starting zoom
+  container: "map", // container ID
+  style: "mapbox://styles/mapbox/streets-v12", // style URL
+  center: [-74.5, 40], // starting position [lng, lat]
+  zoom: 9, // starting zoom
 });
